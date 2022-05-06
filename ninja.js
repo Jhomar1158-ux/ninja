1225bd7a982548e303eb9414c947ce5134c67869
Jhomar1158-ux <jhomarcristianelias@gmail.com>
2022-05-05 20:52:14 -0500

    TAREA sensei

--- ninja.js
+++ ninja.js
@@ -1,3 +1,4 @@
+
 class Ninja{
     constructor(nombre, salud){
         this.nombre=nombre;
@@ -5,18 +6,30 @@ class Ninja{
         this.velocidad=3;
         this.fuerza=3;
     }
-
-
     sayName(){
         console.log(`El ninja se llama: ${this.nombre}`);
     }
-
     showStats(){
         console.log(`Nombre: ${this.nombre}, Fuerza: ${this.fuerza}, Velocidad: ${this.velocidad} y Salud: ${this.salud}`);
     }
-
     drinkSake(){
         this.salud+=10;
+    }
+}
+
+
+
+class Sensei extends Ninja{
+    constructor(nombre){
+        super(nombre,200)
+        this.velocidad=10;
+        this.fuerza=10;
+        this.sabiduria=10;
+    }
+
+    speakWisdom(){
+        super.drinkSake();
+        console.log("Obtuviste más salud!")
     }
 }
 
@@ -25,3 +38,7 @@ ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
+
+const superSensei = new Sensei("Elias")
+superSensei.speakWisdom();
+superSensei.showStats();
